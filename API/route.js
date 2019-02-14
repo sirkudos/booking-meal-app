c3d062f7d9e15910cf71fb677e0917b249146cd5
unknown <attahikechukwu@gmail.com>
2019-02-14 17:15:06 +0100

    installing packages

--- API/route.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const controller = require('./controller');
-const router = express.Router();
-
-// meal routes
-router.get('/', controller.get);
-router.get('/meals', controller.getMeals);
-module.exports = router;
