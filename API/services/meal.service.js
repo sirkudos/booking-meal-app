7075e6e032eb3799b93122d4f5c39db52d528ee9
unknown <attahikechukwu@gmail.com>
2019-02-15 15:01:07 +0100

    creating meal services

---
+++ API/services/meal.service.js
@@ -0,0 +1,35 @@
+import dummyData from '../utils/dummyData';
+
+const MealService = {
+  fetchAllMeal() {
+const validMeals = dummyData.meals.map((meal)=>{
+    const newMeal = newMeal();
+    newMeal.id = meal.id;
+    newMeal.name = meal.name;
+    newMeal.size = meal.size;
+    newMeal.price = meal.price
+});
+  return validMeals;
+  },
+
+
+  addMeal(meal) {
+      const mealLength = dummyData.meals.length;
+      const lastId = dummydata.meal[mealLength-1].id;
+      const newId = lastId + 1;
+      meal.id = newId;
+      dummyData.meals.push(meal);
+      return meal;
+
+
+  },
+
+  getMeal(id) {
+      const meal = dummyData.meals.find(meal => meal.id = id);
+      return meal || {};
+  }
+
+
+};
+
+export default MealService;
---
+++ API/utils/dummyData.js
@@ -0,0 +1,32 @@
+export default {
+  meals: [
+    {
+      id: 1,
+      name: 'Fried Rice',
+      size: 'Medium',
+      price: '450'
+    },
+
+    {
+      id: 2,
+      name: 'Beans',
+      size: 'Small',
+      price: '550'
+    },
+
+    {
+      id: 3,
+      name: 'Garri and Soup',
+      size: 'Large',
+      price: '450',
+    },
+
+    {
+      id: 4,
+      name: 'Okpa',
+      size: 'Medium',
+      price: '450'
+    }
+
+ ]
+}
