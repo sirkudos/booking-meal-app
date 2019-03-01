f41703cb4b15693eb09106a351ecfc15f8f4cecd
unknown <attahikechukwu@gmail.com>
2019-03-01 14:13:16 +0100

    changes in root folder

--- API/.eslintrc.js
+++ API/.eslintrc.js
@@ -3,4 +3,5 @@ module.exports = { "extends": "airbnb-base" };
   "rules": {
     "linebreak-style": 0,
     "global-require": 0,
-    "eslint linebreak-style": [0, "error", "windows"],
+    "eslint linebreak-style": [0, "error", "windows"];
+  }
