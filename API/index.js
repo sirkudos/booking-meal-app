646caeeefb4670f954b1b888a9c8cc62030d375a
unknown <attahikechukwu@gmail.com>
2019-03-07 19:14:15 +0100

    updated index.js

--- API/index.js
+++ API/index.js
@@ -9,11 +9,11 @@ app.use(bodyparser.json());
 
 app.get('/', (req, res) => {
   res.send('testing');
-
-  app.use('/api/v1/meals', mealRoutes);
 });
+
+app.use('/api/v1/meals', mealRoutes);
 app.listen(PORT, () => {
   console.log(`server runinig on port ${PORT}`);
 });
 
-module.exports = app;
+export default app;
